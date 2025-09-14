657956408e61e3c106780fd7f6510aecb5b4932e
onechesss <moskwin.vanya2017@yandex.ru>
2025-09-14 21:25:56 +0300

    refactor: переделал настройку всех UI-свойств (раньше они настраивались в отдельной функции в расширении, а теперь через ленивые свойства, а их добавление на вью и настройку констрейнтов оставил в расширении) [только для PaymentView]

--- FakeNFT/Scenes /Shopping Cart/Payment/PaymentView.swift
+++ FakeNFT/Scenes /Shopping Cart/Payment/PaymentView.swift
@@ -16,14 +16,97 @@ final class PaymentViewController: UIViewController, PaymentViewProtocol {
     var paymentPresenter: PaymentPresenterProtocol?
     
     // MARK: UI Elements
-    private let paymentLabel = UILabel()
-    private lazy var currenciesCollectionView = UICollectionView(frame: .zero, collectionViewLayout: currenciesCollectionViewFlowLayout)
-    private let bottomBackgroundView = UIView()
-    private let payButton = UIButton()
-    private let userAgreementLabel = UILabel()
-    private let userAgreementButton = UIButton()
+    private lazy var paymentLabel: UILabel = {
+        let paymentLabel = UILabel()
+        paymentLabel.translatesAutoresizingMaskIntoConstraints = false
+        paymentLabel.numberOfLines = 1
+        paymentLabel.textAlignment = .center
+        paymentLabel.font = .systemFont(ofSize: 17, weight: .bold)
+        paymentLabel.text = L10n.Payment.title
+        if traitCollection.userInterfaceStyle == .dark {
+            paymentLabel.textColor = .white
+        } else {
+            paymentLabel.textColor = UIColor(hexString: "#1A1B22")
+        }
+        return paymentLabel
+    }()
+    private lazy var currenciesCollectionView: UICollectionView = {
+        let currenciesCollectionView = UICollectionView(frame: .zero, collectionViewLayout: currenciesCollectionViewFlowLayout)
+        currenciesCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        currenciesCollectionView.isScrollEnabled = false
+        currenciesCollectionView.backgroundColor = .clear
+        currenciesCollectionView.register(CurrenciesCollectionViewCell.self, forCellWithReuseIdentifier: "currenciesCollectionViewCell")
+        return currenciesCollectionView
+    }()
+    private lazy var bottomBackgroundView: UIView = {
+        let bottomBackgroundView = UIView()
+        bottomBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        if traitCollection.userInterfaceStyle == .dark {
+            bottomBackgroundView.backgroundColor = UIColor(hexString: "#2C2C2E")
+        } else {
+            bottomBackgroundView.backgroundColor = UIColor(hexString: "#F7F7F8")
+        }
+        bottomBackgroundView.layer.masksToBounds = true
+        bottomBackgroundView.layer.cornerRadius = 12
+        bottomBackgroundView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        return bottomBackgroundView
+    }()
+    private lazy var payButton: UIButton = {
+        let payButton = UIButton()
+        payButton.translatesAutoresizingMaskIntoConstraints = false
+        payButton.layer.masksToBounds = true
+        payButton.layer.cornerRadius = 16
+        payButton.setTitle(L10n.Payment.pay, for: .normal)
+        payButton.titleLabel?.font = .systemFont(ofSize: 17, weight: .bold)
+        if traitCollection.userInterfaceStyle == .dark {
+            payButton.setTitleColor(UIColor(hexString: "#1A1B22"), for: .normal)
+            payButton.backgroundColor = .white
+        } else {
+            payButton.setTitleColor(.white, for: .normal)
+            payButton.backgroundColor = UIColor(hexString: "#1A1B22")
+        }
+        return payButton
+    }()
+    private lazy var userAgreementLabel: UILabel = {
+        let userAgreementLabel = UILabel()
+        userAgreementLabel.translatesAutoresizingMaskIntoConstraints = false
+        userAgreementLabel.numberOfLines = 1
+        userAgreementLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        userAgreementLabel.textAlignment = .left
+        if Locale.current.languageCode == "ru" {
+            userAgreementLabel.text = "Совершая покупку, вы соглашаетесь с условиями"
+        } else {
+            userAgreementLabel.text = "By making a purchase, you agree to the terms of the"
+        }
+        if traitCollection.userInterfaceStyle == .dark {
+            userAgreementLabel.textColor = .white
+        } else {
+            userAgreementLabel.textColor = UIColor(hexString: "#1A1B22")
+        }
+        return userAgreementLabel
+    }()
+    private let userAgreementButton: UIButton = {
+        let userAgreementButton = UIButton()
+        userAgreementButton.translatesAutoresizingMaskIntoConstraints = false
+        userAgreementButton.titleLabel?.font = .systemFont(ofSize: 13, weight: .regular)
+        userAgreementButton.setTitleColor(UIColor(hexString: "#0A84FF"), for: .normal)
+        userAgreementButton.contentHorizontalAlignment = .left
+        if Locale.current.languageCode == "ru" {
+            userAgreementButton.setTitle("Пользовательского соглашения", for: .normal)
+        } else {
+            userAgreementButton.setTitle("User agreement", for: .normal)
+        }
+        userAgreementButton.addTarget(userAgreementButton.self, action: #selector(userAgreementButtonTapped), for: .touchUpInside)
+        return userAgreementButton
+    }()
     
-    private let currenciesCollectionViewFlowLayout = UICollectionViewFlowLayout()
+    private lazy var currenciesCollectionViewFlowLayout: UICollectionViewFlowLayout = {
+        let currenciesCollectionViewFlowLayout = UICollectionViewFlowLayout()
+        currenciesCollectionViewFlowLayout.itemSize = CGSize(width: 168, height: 46)
+        currenciesCollectionViewFlowLayout.minimumLineSpacing = 7
+        currenciesCollectionViewFlowLayout.minimumInteritemSpacing = 7
+        return currenciesCollectionViewFlowLayout
+    }()
     private lazy var currenciesCollectionViewDiffableDataSource = UICollectionViewDiffableDataSource<Int, Currency>(collectionView: currenciesCollectionView) { [weak self] collectionView, indexPath, itemIdentifier in
         guard let self,
               let cell = self.currenciesCollectionView.dequeueReusableCell(withReuseIdentifier: "currenciesCollectionViewCell", for: indexPath) as? CurrenciesCollectionViewCell
@@ -36,7 +119,6 @@ final class PaymentViewController: UIViewController, PaymentViewProtocol {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        setUpCurrenciesCollectionViewFlowLayout()
         paymentPresenter?.loadCurrenciesFromServer()
         ProgressHUDProvider.showProgressHUD()
     }
@@ -57,12 +139,6 @@ final class PaymentViewController: UIViewController, PaymentViewProtocol {
         ProgressHUDProvider.dismissProgressHUD()
     }
     
-    private func setUpCurrenciesCollectionViewFlowLayout() {
-        currenciesCollectionViewFlowLayout.itemSize = CGSize(width: 168, height: 46)
-        currenciesCollectionViewFlowLayout.minimumLineSpacing = 7
-        currenciesCollectionViewFlowLayout.minimumInteritemSpacing = 7
-    }
-    
     // MARK: UI Actions
     @objc private func userAgreementButtonTapped() {
         guard let url = URL(string: "https://yandex.ru/legal/practicum_termsofuse") else { return }
@@ -75,12 +151,55 @@ final class PaymentViewController: UIViewController, PaymentViewProtocol {
 // MARK: setupView
 private extension PaymentViewController {
     private func setupView() {
-        setUpPaymentLabel()
-        setUpCurrenciesCollectionView()
-        setUpBottomBackgroundView()
-        setUpPayButton()
-        setUpUserAgreementLabel()
-        setUpUserAgreementButton()
+        view.addSubview(paymentLabel)
+        view.addSubview(currenciesCollectionView)
+        view.addSubview(bottomBackgroundView)
+        view.addSubview(payButton)
+        view.addSubview(userAgreementLabel)
+        view.addSubview(userAgreementButton)
+        
+        if let navigationBar = navigationController?.navigationBar, navigationBar.superview != view {
+            view.addSubview(navigationBar)
+        }
+        guard let navigationBarCenterYAnchor = navigationController?.navigationBar.centerYAnchor else { return }
+        
+        NSLayoutConstraint.activate([
+            paymentLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            paymentLabel.centerYAnchor.constraint(equalTo: navigationBarCenterYAnchor),
+            paymentLabel.widthAnchor.constraint(equalToConstant: 261),
+            paymentLabel.heightAnchor.constraint(equalToConstant: 22)
+        ])
+        NSLayoutConstraint.activate([
+            currenciesCollectionView.topAnchor.constraint(equalTo: paymentLabel.bottomAnchor, constant: 30),
+            currenciesCollectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            currenciesCollectionView.widthAnchor.constraint(equalToConstant: 343),
+            currenciesCollectionView.heightAnchor.constraint(equalToConstant: 205)
+        ])
+        NSLayoutConstraint.activate([
+            bottomBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomBackgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            bottomBackgroundView.heightAnchor.constraint(equalToConstant: 186)
+        ])
+        NSLayoutConstraint.activate([
+            payButton.widthAnchor.constraint(equalToConstant: 343),
+            payButton.heightAnchor.constraint(equalToConstant: 60),
+            payButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            payButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+        ])
+        NSLayoutConstraint.activate([
+            userAgreementLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            userAgreementLabel.topAnchor.constraint(equalTo: bottomBackgroundView.topAnchor, constant: 16),
+            userAgreementLabel.widthAnchor.constraint(equalToConstant: 380),
+            userAgreementLabel.heightAnchor.constraint(equalToConstant: 18)
+        ])
+        NSLayoutConstraint.activate([
+            userAgreementButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            userAgreementButton.topAnchor.constraint(equalTo: userAgreementLabel.bottomAnchor),
+            userAgreementButton.widthAnchor.constraint(equalToConstant: 202),
+            userAgreementButton.heightAnchor.constraint(equalToConstant: 26)
+        ])
+        
         if traitCollection.userInterfaceStyle == .dark {
             view.backgroundColor = UIColor(hexString: "#1A1B22")
             navigationController?.navigationBar.tintColor = .white
@@ -88,129 +207,6 @@ private extension PaymentViewController {
             view.backgroundColor = .white
             navigationItem.backBarButtonItem?.tintColor = UIColor(hexString: "#1A1B22")
         }
-    }
-    
-    private func setUpPaymentLabel() {
-        paymentLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(paymentLabel)
-        if let navigationBar = navigationController?.navigationBar, navigationBar.superview != view {
-            view.addSubview(navigationBar)
-        }
-        guard let navigationBarCenterYAnchor = navigationController?.navigationBar.centerYAnchor else { return }
-        NSLayoutConstraint.activate([
-            paymentLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            paymentLabel.centerYAnchor.constraint(equalTo: navigationBarCenterYAnchor),
-            paymentLabel.widthAnchor.constraint(equalToConstant: 261),
-            paymentLabel.heightAnchor.constraint(equalToConstant: 22)
-        ])
-        paymentLabel.numberOfLines = 1
-        paymentLabel.textAlignment = .center
-        paymentLabel.font = .systemFont(ofSize: 17, weight: .bold)
-        paymentLabel.text = L10n.Payment.title
-        if traitCollection.userInterfaceStyle == .dark {
-            paymentLabel.textColor = .white
-        } else {
-            paymentLabel.textColor = UIColor(hexString: "#1A1B22")
-        }
-    }
-    
-    private func setUpCurrenciesCollectionView() {
-        currenciesCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(currenciesCollectionView)
-        NSLayoutConstraint.activate([
-            currenciesCollectionView.topAnchor.constraint(equalTo: paymentLabel.bottomAnchor, constant: 30),
-            currenciesCollectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            currenciesCollectionView.widthAnchor.constraint(equalToConstant: 343),
-            currenciesCollectionView.heightAnchor.constraint(equalToConstant: 205)
-        ])
-        currenciesCollectionView.isScrollEnabled = false
-        currenciesCollectionView.backgroundColor = .clear
-        currenciesCollectionView.register(CurrenciesCollectionViewCell.self, forCellWithReuseIdentifier: "currenciesCollectionViewCell")
-    }
-    
-    private func setUpBottomBackgroundView() {
-        bottomBackgroundView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(bottomBackgroundView)
-        NSLayoutConstraint.activate([
-            bottomBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            bottomBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            bottomBackgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            bottomBackgroundView.heightAnchor.constraint(equalToConstant: 186)
-        ])
-        if traitCollection.userInterfaceStyle == .dark {
-            bottomBackgroundView.backgroundColor = UIColor(hexString: "#2C2C2E")
-        } else {
-            bottomBackgroundView.backgroundColor = UIColor(hexString: "#F7F7F8")
-        }
-        bottomBackgroundView.layer.masksToBounds = true
-        bottomBackgroundView.layer.cornerRadius = 12
-        bottomBackgroundView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-    }
-    
-    private func setUpPayButton() {
-        payButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(payButton)
-        NSLayoutConstraint.activate([
-            payButton.widthAnchor.constraint(equalToConstant: 343),
-            payButton.heightAnchor.constraint(equalToConstant: 60),
-            payButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            payButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
-        ])
-        payButton.layer.masksToBounds = true
-        payButton.layer.cornerRadius = 16
-        payButton.setTitle(L10n.Payment.pay, for: .normal)
-        payButton.titleLabel?.font = .systemFont(ofSize: 17, weight: .bold)
-        if traitCollection.userInterfaceStyle == .dark {
-            payButton.setTitleColor(UIColor(hexString: "#1A1B22"), for: .normal)
-            payButton.backgroundColor = .white
-        } else {
-            payButton.setTitleColor(.white, for: .normal)
-            payButton.backgroundColor = UIColor(hexString: "#1A1B22")
-        }
-    }
-    
-    private func setUpUserAgreementLabel() {
-        userAgreementLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(userAgreementLabel)
-        NSLayoutConstraint.activate([
-            userAgreementLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            userAgreementLabel.topAnchor.constraint(equalTo: bottomBackgroundView.topAnchor, constant: 16),
-            userAgreementLabel.widthAnchor.constraint(equalToConstant: 380),
-            userAgreementLabel.heightAnchor.constraint(equalToConstant: 18)
-        ])
-        userAgreementLabel.numberOfLines = 1
-        userAgreementLabel.font = .systemFont(ofSize: 13, weight: .regular)
-        userAgreementLabel.textAlignment = .left
-        if Locale.current.languageCode == "ru" {
-            userAgreementLabel.text = "Совершая покупку, вы соглашаетесь с условиями"
-        } else {
-            userAgreementLabel.text = "By making a purchase, you agree to the terms of the"
-        }
-        if traitCollection.userInterfaceStyle == .dark {
-            userAgreementLabel.textColor = .white
-        } else {
-            userAgreementLabel.textColor = UIColor(hexString: "#1A1B22")
-        }
-    }
-    
-    private func setUpUserAgreementButton() {
-        userAgreementButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(userAgreementButton)
-        NSLayoutConstraint.activate([
-            userAgreementButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            userAgreementButton.topAnchor.constraint(equalTo: userAgreementLabel.bottomAnchor),
-            userAgreementButton.widthAnchor.constraint(equalToConstant: 202),
-            userAgreementButton.heightAnchor.constraint(equalToConstant: 26)
-        ])
-        userAgreementButton.titleLabel?.font = .systemFont(ofSize: 13, weight: .regular)
-        userAgreementButton.setTitleColor(UIColor(hexString: "#0A84FF"), for: .normal)
-        userAgreementButton.contentHorizontalAlignment = .left
-        if Locale.current.languageCode == "ru" {
-            userAgreementButton.setTitle("Пользовательского соглашения", for: .normal)
-        } else {
-            userAgreementButton.setTitle("User agreement", for: .normal)
-        }
-        userAgreementButton.addTarget(self, action: #selector(userAgreementButtonTapped), for: .touchUpInside)
     }
 }
 
