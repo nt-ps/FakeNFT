89c2a55e5ae06c9a9605571035ab6431100d80a5
onechesss <moskwin.vanya2017@yandex.ru>
2025-09-14 22:36:16 +0300

    refactor: проведены те же операции с кастомными ячейками для таблицы с NFT и для коллекции со способами оплаты (рефакторинг на ленивую иницииализацию и рефакторинг на цвета из Assets)

--- FakeNFT/Scenes /Shopping Cart/Helpers (shopping cart)/UI Helpers for shopping cart/CurrenciesCollectionViewCell.swift
+++ FakeNFT/Scenes /Shopping Cart/Helpers (shopping cart)/UI Helpers for shopping cart/CurrenciesCollectionViewCell.swift
@@ -9,11 +9,48 @@ import UIKit
 
 final class CurrenciesCollectionViewCell: UICollectionViewCell {
     // MARK: UI Elements
-    private let cellBackgroundView = UIView()
-    private let currencyBackgroundView = UIView()
-    private let currencyImageView = UIImageView()
-    private let currencyNameLabel = UILabel()
-    private let currencyAbbreviationLabel = UILabel()
+    private lazy var cellBackgroundView: UIView = {
+        let cellBackgroundView = UIView()
+        cellBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        cellBackgroundView.layer.masksToBounds = true
+        cellBackgroundView.layer.cornerRadius = 12
+        cellBackgroundView.backgroundColor = .AppColors.lightGray
+        return cellBackgroundView
+    }()
+    private lazy var currencyBackgroundView: UIView = {
+        let currencyBackgroundView = UIView()
+        currencyBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        currencyBackgroundView.layer.masksToBounds = true
+        currencyBackgroundView.layer.cornerRadius = 6
+        currencyBackgroundView.backgroundColor = UIColor(hexString: "#1A1B22")
+        return currencyBackgroundView
+    }()
+    private lazy var currencyImageView: UIImageView = {
+        let currencyImageView = UIImageView()
+        currencyImageView.translatesAutoresizingMaskIntoConstraints = false
+        currencyImageView.backgroundColor = .clear
+        return currencyImageView
+    }()
+    private lazy var currencyNameLabel: UILabel = {
+        let currencyNameLabel = UILabel()
+        currencyNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        currencyNameLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        currencyNameLabel.numberOfLines = 1
+        currencyNameLabel.textAlignment = .left
+        currencyNameLabel.backgroundColor = .clear
+        currencyNameLabel.textColor = .AppColors.black
+        return currencyNameLabel
+    }()
+    private lazy var currencyAbbreviationLabel: UILabel = {
+        let currencyAbbreviationLabel = UILabel()
+        currencyAbbreviationLabel.translatesAutoresizingMaskIntoConstraints = false
+        currencyAbbreviationLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        currencyAbbreviationLabel.numberOfLines = 1
+        currencyAbbreviationLabel.textAlignment = .left
+        currencyAbbreviationLabel.backgroundColor = .clear
+        currencyAbbreviationLabel.textColor = UIColor(hexString: "#1C9F00")
+        return currencyAbbreviationLabel
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -35,108 +72,47 @@ final class CurrenciesCollectionViewCell: UICollectionViewCell {
 // MARK: setup view
 private extension CurrenciesCollectionViewCell {
     private func setupView() {
-        setUpCellBackgroundView()
-        setUpCurrencyBackgroundView()
-        setUpCurrencyImageView()
-        setUpCurrencyNameLabel()
-        setUpCurrencyAbbreviationLabel()
         backgroundColor = .clear
-    }
-    
-    private func setUpCellBackgroundView() {
-        cellBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
         contentView.addSubview(cellBackgroundView)
+        contentView.addSubview(currencyBackgroundView)
+        contentView.addSubview(currencyImageView)
+        contentView.addSubview(currencyNameLabel)
+        contentView.addSubview(currencyAbbreviationLabel)
+        
         NSLayoutConstraint.activate([
             cellBackgroundView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             cellBackgroundView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             cellBackgroundView.topAnchor.constraint(equalTo: contentView.topAnchor),
             cellBackgroundView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
-        cellBackgroundView.layer.masksToBounds = true
-        cellBackgroundView.layer.cornerRadius = 12
-        if traitCollection.userInterfaceStyle == .dark {
-            cellBackgroundView.backgroundColor = UIColor(hexString: "#2C2C2E")
-        } else {
-            cellBackgroundView.backgroundColor = UIColor(hexString: "#F7F7F8")
-        }
-    }
-    
-    private func setUpCurrencyBackgroundView() {
-        currencyBackgroundView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(currencyBackgroundView)
+        
         NSLayoutConstraint.activate([
             currencyBackgroundView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             currencyBackgroundView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
             currencyBackgroundView.widthAnchor.constraint(equalToConstant: 36),
             currencyBackgroundView.heightAnchor.constraint(equalToConstant: 36)
         ])
-        currencyBackgroundView.layer.masksToBounds = true
-        currencyBackgroundView.layer.cornerRadius = 6
-        currencyBackgroundView.backgroundColor = UIColor(hexString: "#1A1B22")
-    }
-    
-    private func setUpCurrencyImageView () {
-        currencyImageView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(currencyImageView)
+        
         NSLayoutConstraint.activate([
             currencyImageView.centerYAnchor.constraint(equalTo: currencyBackgroundView.centerYAnchor),
             currencyImageView.centerXAnchor.constraint(equalTo: currencyBackgroundView.centerXAnchor),
             currencyImageView.widthAnchor.constraint(equalToConstant: 31.5),
             currencyImageView.heightAnchor.constraint(equalToConstant: 31.5)
         ])
-        currencyImageView.backgroundColor = .clear
-    }
-    
-    private func setUpCurrencyNameLabel() {
-        currencyNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(currencyNameLabel)
+        
         NSLayoutConstraint.activate([
             currencyNameLabel.leadingAnchor.constraint(equalTo: currencyBackgroundView.trailingAnchor, constant: 4),
             currencyNameLabel.topAnchor.constraint(equalTo: currencyBackgroundView.topAnchor),
             currencyNameLabel.widthAnchor.constraint(equalToConstant: 100),
             currencyNameLabel.heightAnchor.constraint(equalToConstant: 18)
         ])
-        currencyNameLabel.font = .systemFont(ofSize: 13, weight: .regular)
-        currencyNameLabel.numberOfLines = 1
-        currencyNameLabel.textAlignment = .left
-        currencyNameLabel.text = "sfafwgtaha"
-        currencyNameLabel.backgroundColor = .clear
-        if traitCollection.userInterfaceStyle == .dark {
-            currencyNameLabel.textColor = .white
-        } else {
-            currencyNameLabel.textColor = UIColor(hexString: "#1A1B22")
-        }
-    }
-    
-    private func setUpCurrencyAbbreviationLabel() {
-        currencyAbbreviationLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(currencyAbbreviationLabel)
+        
         NSLayoutConstraint.activate([
             currencyAbbreviationLabel.topAnchor.constraint(equalTo: currencyNameLabel.bottomAnchor),
             currencyAbbreviationLabel.leadingAnchor.constraint(equalTo: currencyBackgroundView.trailingAnchor, constant: 4),
             currencyNameLabel.widthAnchor.constraint(equalToConstant: 100),
             currencyAbbreviationLabel.heightAnchor.constraint(equalToConstant: 18)
         ])
-        currencyAbbreviationLabel.font = .systemFont(ofSize: 13, weight: .regular)
-        currencyAbbreviationLabel.numberOfLines = 1
-        currencyAbbreviationLabel.textAlignment = .left
-        currencyAbbreviationLabel.text = "tyjk"
-        currencyAbbreviationLabel.backgroundColor = .clear
-        currencyAbbreviationLabel.textColor = UIColor(hexString: "#1C9F00")
-    }
-}
-
-// MARK: Dark theme implementation
-extension CurrenciesCollectionViewCell {
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        if traitCollection.userInterfaceStyle == .dark {
-            cellBackgroundView.backgroundColor = UIColor(hexString: "#2C2C2E")
-            currencyNameLabel.textColor = .white
-            
-        } else {
-            cellBackgroundView.backgroundColor = UIColor(hexString: "#F7F7F8")
-            currencyNameLabel.textColor = UIColor(hexString: "#1A1B22")
-        }
     }
 }
--- FakeNFT/Scenes /Shopping Cart/Helpers (shopping cart)/UI Helpers for shopping cart/NFTTableViewCell.swift
+++ FakeNFT/Scenes /Shopping Cart/Helpers (shopping cart)/UI Helpers for shopping cart/NFTTableViewCell.swift
@@ -16,12 +16,48 @@ final class NFTTableViewCell: UITableViewCell {
     weak var delegate: NFTTableViewCellDelegate?
     
     // MARK: UI Elements
-    private let NFTNameLabel = UILabel()
-    private let ratingImageView = UIImageView()
-    private let priceLabel = UILabel()
-    private let priceValueLabel = UILabel()
-    private let NFTImageView = UIImageView()
-    private let deleteFromCartButton = UIButton()
+    private lazy var NFTNameLabel: UILabel = {
+        let NFTNameLabel = UILabel()
+        NFTNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NFTNameLabel.font = .systemFont(ofSize: 17, weight: .bold)
+        NFTNameLabel.textColor = .AppColors.black
+        return NFTNameLabel
+    }()
+    private lazy var ratingImageView: UIImageView = {
+        let ratingImageView = UIImageView()
+        ratingImageView.translatesAutoresizingMaskIntoConstraints = false
+        ratingImageView.image = UIImage(resource: .Icons.Rating.three)
+        return ratingImageView
+    }()
+    private lazy var priceLabel: UILabel = {
+        let priceLabel = UILabel()
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceLabel.text = L10n.Cart.price
+        priceLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        priceLabel.textColor = .AppColors.black
+        return priceLabel
+    }()
+    private lazy var priceValueLabel: UILabel = {
+        let priceValueLabel = UILabel()
+        priceValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceValueLabel.textColor = .AppColors.black
+        return priceValueLabel
+    }()
+    private lazy var NFTImageView: UIImageView = {
+        let NFTImageView = UIImageView()
+        NFTImageView.translatesAutoresizingMaskIntoConstraints = false
+        NFTImageView.layer.cornerRadius = 12
+        NFTImageView.layer.masksToBounds = true
+        return NFTImageView
+    }()
+    private lazy var deleteFromCartButton: UIButton = {
+        let deleteFromCartButton = UIButton()
+        deleteFromCartButton.translatesAutoresizingMaskIntoConstraints = false
+        deleteFromCartButton.setImage(UIImage(resource: .Icons.Cart.delete), for: .normal)
+        deleteFromCartButton.tintColor = .AppColors.black
+        deleteFromCartButton.addTarget(self, action: #selector(deleteFromCartButtonTapped), for: .touchUpInside)
+        return deleteFromCartButton
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -65,111 +101,40 @@ final class NFTTableViewCell: UITableViewCell {
 // MARK: setup cell view
 private extension NFTTableViewCell {
     private func setupView() {
-        setUpNFTImageView()
-        setUpNFTNameLabel()
-        setUpRatingImageView()
-        setUpPriceLabel()
-        setUpPriceValueLabel()
-        setUpDeleteFromCartButton()
         backgroundColor = .clear
-    }
-    
-    private func setUpNFTNameLabel() {
-        NFTNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(NFTNameLabel)
-        NSLayoutConstraint.activate([
-            NFTNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 24),
-            NFTNameLabel.leadingAnchor.constraint(equalTo: NFTImageView.trailingAnchor, constant: 20),
-        ])
-        NFTNameLabel.font = .systemFont(ofSize: 17, weight: .bold)
-        NFTNameLabel.text = "NFTs name"
-    }
-    
-    private func setUpRatingImageView() {
-        ratingImageView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(ratingImageView)
-        NSLayoutConstraint.activate([
-            ratingImageView.topAnchor.constraint(equalTo: NFTNameLabel.bottomAnchor, constant: 4),
-            ratingImageView.leadingAnchor.constraint(equalTo: NFTImageView.trailingAnchor, constant: 20)
-        ])
-        ratingImageView.image = UIImage(resource: .Icons.Rating.three)
-    }
-    
-    private func setUpNFTImageView() {
-        NFTImageView.translatesAutoresizingMaskIntoConstraints = false
+        
         contentView.addSubview(NFTImageView)
+        contentView.addSubview(NFTNameLabel)
+        contentView.addSubview(ratingImageView)
+        contentView.addSubview(priceLabel)
+        contentView.addSubview(priceValueLabel)
+        contentView.addSubview(deleteFromCartButton)
+        
         NSLayoutConstraint.activate([
             NFTImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             NFTImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             NFTImageView.widthAnchor.constraint(equalToConstant: 108),
             NFTImageView.heightAnchor.constraint(equalToConstant: 108)
         ])
-        NFTImageView.layer.cornerRadius = 12
-        NFTImageView.layer.masksToBounds = true
-    }
-    
-    private func setUpPriceLabel() {
-        priceLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(priceLabel)
+        NSLayoutConstraint.activate([
+            NFTNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 24),
+            NFTNameLabel.leadingAnchor.constraint(equalTo: NFTImageView.trailingAnchor, constant: 20),
+        ])
+        NSLayoutConstraint.activate([
+            ratingImageView.topAnchor.constraint(equalTo: NFTNameLabel.bottomAnchor, constant: 4),
+            ratingImageView.leadingAnchor.constraint(equalTo: NFTImageView.trailingAnchor, constant: 20)
+        ])
         NSLayoutConstraint.activate([
             priceLabel.leadingAnchor.constraint(equalTo: NFTImageView.trailingAnchor, constant: 20),
             priceLabel.topAnchor.constraint(equalTo: ratingImageView.bottomAnchor, constant: 12)
         ])
-        priceLabel.text = L10n.Cart.price
-        priceLabel.font = .systemFont(ofSize: 13, weight: .regular)
-        if traitCollection.userInterfaceStyle == .dark {
-            priceLabel.textColor = .white
-        } else {
-            priceLabel.textColor = UIColor(hexString: "#1A1B22")
-        }
-    }
-    
-    private func setUpPriceValueLabel() {
-        priceValueLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(priceValueLabel)
         NSLayoutConstraint.activate([
             priceValueLabel.leadingAnchor.constraint(equalTo: NFTImageView.trailingAnchor, constant: 20),
             priceValueLabel.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 2)
         ])
-        priceValueLabel.text = "N $$$$$"
-        if traitCollection.userInterfaceStyle == .dark {
-            priceValueLabel.textColor = .white
-        } else {
-            priceValueLabel.textColor = UIColor(hexString: "#1A1B22")
-        }
-    }
-    
-    private func setUpDeleteFromCartButton() {
-        deleteFromCartButton.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(deleteFromCartButton)
         NSLayoutConstraint.activate([
             deleteFromCartButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             deleteFromCartButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
         ])
-        deleteFromCartButton.setImage(UIImage(resource: .Icons.Cart.delete), for: .normal)
-        if traitCollection.userInterfaceStyle == .dark {
-            deleteFromCartButton.tintColor = .white
-        } else {
-            deleteFromCartButton.tintColor = UIColor(hexString: "#1A1B22")
-        }
-        deleteFromCartButton.addTarget(self, action: #selector(deleteFromCartButtonTapped), for: .touchUpInside)
-    }
-}
-
-// MARK: Dark theme implementation
-extension NFTTableViewCell {
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        if traitCollection.userInterfaceStyle == .dark {
-            NFTNameLabel.textColor = .white
-            priceLabel.textColor = .white
-            priceValueLabel.textColor = .white
-            deleteFromCartButton.tintColor = .white
-        } else {
-            NFTNameLabel.textColor = UIColor(hexString: "#1A1B22")
-            priceLabel.textColor = UIColor(hexString: "#1A1B22")
-            priceValueLabel.textColor = UIColor(hexString: "#1A1B22")
-            deleteFromCartButton.tintColor = UIColor(hexString: "#1A1B22")
-        }
     }
 }
